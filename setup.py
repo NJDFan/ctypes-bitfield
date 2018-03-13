ae76b1dcfef7ecc90bd1900735b94ddee41a6376
Rob Gaddi <rgaddi@highlandtechnology.com>
2018-03-13 13:32:43 -0700

    Added GitHub URL to setup.py.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 	long_description=long_description,
 
 	# The project's main homepage.
-	# url='https://github.com/pypa/sampleproject',
+	url='https://github.com/NJDFan/ctypes-bitfield/',
 
 	# Author details
 	author='Rob Gaddi',
